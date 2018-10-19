16932ed8684303c7a75e13631b99f6ebc9e0545b
Edmund Ochieng <edmund.ochieng@rackspace.com>
2018-10-18 22:38:12 -0500

    Using stravalib to auth against strava api V3

--- src/baisikeli.py
+++ src/baisikeli.py
@@ -1,20 +1,55 @@
-import requests
 from configparser import ConfigParser
+from stravalib import Client
+from flask import Flask
+import json, sys
+import requests
+
+class Configuration:
+    def __init__(self, config_file):
+        self.config_file = config_file
+        self.app_config = ConfigParser()
+        self.config_check()
+
+    def config_check(self):
+        try:
+            f = open(self.config_file, 'r')
+        except FileNotFoundError:
+            self.create_basic_config()
+            print("New config created at %s. Update with app settings before retrying" % self.config_file)
+            sys.exit(1)
+
+    def create_basic_config(self):
+        default_config = self.app_config
+        default_config.add_section('strava')
+        default_config.set('strava', 'client_id', '1234')
+        default_config.set('strava', 'client_secret', 'rand0mstr1ng')
+        default_config.set('strava', 'redirect_uri', 'http://www.yourdomain.com/authorization')
+        with open(self.config_file, 'w') as application_config:
+            default_config.write(application_config)
+
+    def get_strava_app_data(self):
+        app_data = self.app_config
+        app_data.read(self.config_file)
+        return json.dumps({
+                'client_id': app_data['strava']['client_id'],
+                'client_secret': app_data['strava']['client_secret'],
+                'redirect_uri': app_data['strava']['redirect_uri']
+                })
+
+
 
 class Strava:
 
     def __init__(self):
-        self.strava_api_config = 'baisikeli.conf'
-        strava_conf = ConfigParser()
-        
+        self.api_client = Client()
+        config = Configuration('baisikeli.conf')
+        self.app_data = json.loads(config.get_strava_app_data())
 
-    def default_strava_api_config(self):
-        default_conf = ConfigParser()
-        default_conf.add_section('strava')
-        default_conf.set('strava', 'client_id', '1234')
-        default_conf.set('strava', 'redirect_uri', 'http://www.domain.com/strava/authorization')
-        with open(self.strava_api_config, 'w') as config:
-            default_conf.write(config)
+    def get_authorization_url(self):
+        return self.api_client.authorization_url(client_id=self.app_data['client_id'], redirect_uri=self.app_data['redirect_uri'])
+
+    def get_access_token(self, code):
+        return self.api_client.exchange_code_for_token(client_id=self.app_data['client_id'], client_secret=self.app_data['client_secret'], code=code)
 
     def get_cyclist_info(self):
         pass
@@ -25,4 +60,5 @@ class Strava:
 
 if __name__ == '__main__':
     strava = Strava()
-    strava.default_strava_api_config()
+    url = strava.get_authorization_url()
+    print(url)
