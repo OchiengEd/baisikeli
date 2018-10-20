0b86431b8df802341b3637c3e0fa082ff61e135d
Edmund Ochieng <ochienged@gmail.com>
2018-10-19 19:49:21 -0500

    Implement function to pull user information from strava

--- src/baisikeli.py
+++ src/baisikeli.py
@@ -37,13 +37,13 @@ class Configuration:
                 })
 
 
-
 class Strava:
 
     def __init__(self):
         self.api_client = Client()
         config = Configuration('baisikeli.conf')
         self.app_data = json.loads(config.get_strava_app_data())
+        self.access_code = None
 
     def get_authorization_url(self):
         return self.api_client.authorization_url(client_id=self.app_data['client_id'], redirect_uri=self.app_data['redirect_uri'])
@@ -52,14 +52,14 @@ class Strava:
         return self.api_client.exchange_code_for_token(client_id=self.app_data['client_id'], client_secret=self.app_data['client_secret'], code=code)
 
     def get_cyclist_info(self, access_code, athlete_id = None):
-        headers = {'Authorization': 'Bearer: {}'.format(access_code)}
-        url = 'https://www.strava.com/api/v3/athlete' if access_code is None \
-            else 'https://www.strava.com/api/v3/athlete/{}'.format(athlete_id)
+        headers = {'Authorization': 'Bearer {}'.format(access_code)}
+        url = 'https://www.strava.com/api/v3/athlete'
+        if athlete_id is not None:
+            url = ''.join([url, '/', access_code])
 
-        print(url)
-        # request = requests.get(url, headers=headers)
-        # if request.status_code == 200:
-        #     print(request.content)
+        request = requests.get(url, headers=headers)
+        if request.status_code == 200:
+            return request.json()
 
 
 
@@ -69,4 +69,3 @@ class Strava:
 
 if __name__ == '__main__':
     strava = Strava()
-    strava.get_cyclist_info('access_code', '1237')
