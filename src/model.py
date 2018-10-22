64db47637ea136ed5d7af8a2b2338ef70a72486a
Edmund Ochieng <edmund.ochieng@rackspace.com>
2018-10-22 14:06:51 -0500

    Pulling database options from the Configuration class

--- src/model.py
+++ src/model.py
@@ -1,22 +1,25 @@
 from pymongo import MongoClient
+from settings import Configuration
+import json
 
 class DataStore:
 
 	def __init__(self):
+		self.configs = Configuration('baisikeli.conf')
 		self.db = self.connect()
 
 	def connect(self):
-		uri = 'mongodb://localhost:27017/'
-		client = MongoClient(uri)
-		return client['baisikeli']
+		database = json.loads(self.configs.get_db_connection_string())
+		client = MongoClient(database['connection_string'])
+		return client[database['db']]
 
 	def add_strava_athlete(self, strava_athlete):
 		athlete_collection = self.db['athlete']
 		athlete_collection.insert(strava_athlete)
 
-	def search_strava_athlete(self):
+	def search_strava_athlete(self, strava_username):
 		athlete_collection = self.db['athlete']
-		return athlete_collection.find({'':''})
+		return athlete_collection.find({'username': strava_username })
 
 	def show_collections(self):
 		return self.db.list_collection_names()
