45501c1330d8c457e25f43e6d20d8416d160bc9f
Edmund Ochieng <edmund.ochieng@rackspace.com>
2018-11-08 13:20:30 -0600

    Fixed erroneous call of the user_collection

--- src/model.py
+++ src/model.py
@@ -11,7 +11,7 @@ class DataStore:
 	def connect(self):
 		database = json.loads(self.configs.get_db_connection_string())
 		client = MongoClient(database['connection_string'])
-		return client[database['db']]
+		return client.get_database()
 
 	def add_strava_athlete(self, strava_athlete):
 		athlete_collection = self.db['athletes']
@@ -32,11 +32,16 @@ class Auth_Model(DataStore):
 
 	def __init__(self):
 		super().__init__()
-		user_collection = self.db['user']
+		self.user_collection = self.db['user']
 
 	def create_user_account(self, user):
-		user_collection.insert(user)
+		self.user_collection.insert(user)
 
-	def authenticate_user(self, user):
-		input_password = user['password']
-		user_collection.find({'username': user['username'] })
+	def get_user(self, email_address):
+		return self.user_collection.find_one({'email': email_address })
+
+
+if __name__ == '__main__':
+	user = Auth_Model()
+	print(user.get_user('ochienged@gmail.com'))
+	# user.create_user_account({'lastname': 'Ochieng', 'firstname': 'Edmund', 'password': 'pbkdf2:sha256:50000$l8WBC6tV$fc34765f759137dcd167aa44fc3fe629376322d96d782906cc6376366c10c7f3', 'email': 'ochienged@gmail.com'})
