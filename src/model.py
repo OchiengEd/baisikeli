6f084f2ae0c5082e5d4aa53f7d95737dd9fe2bff
Edmund Ochieng <ochienged@gmail.com>
2018-11-14 20:23:07 -0600

    Broke Strava_Model to be separate

--- src/model.py
+++ src/model.py
@@ -17,20 +17,21 @@ class DataStore:
 		except OperationFailure:
 			print("Error connecting to database. \nPlease check credentials in config file")
 
+class Strava_Model(DataStore):
+
+	def __init__(self):
+		super().__init__()
+		self.athletes_collection = self.db['athletes']
+		self.activities_collection = self.db['activities']
+
 	def add_strava_athlete(self, strava_athlete):
-		athlete_collection = self.db['athletes']
-		athlete_collection.insert(strava_athlete)
+		self.athletes_collection.insert(strava_athlete)
 
-	def search_strava_athlete(self, strava_username):
-		athlete_collection = self.db['athletes']
-		return athlete_collection.find({'username': strava_username })
-
-	def show_collections(self):
-		return self.db.list_collection_names()
+	def search_strava_athlete(self, email_address):
+		return self.athletes_collection.find({'email': email_address })
 
 	def store_activities(self, activities):
-		activities_collection = self.db['activities']
-		activities_collection.insert_many(activities['activities'])
+		self.activities_collection.insert_many(activities['activities'])
 
 class Auth_Model(DataStore):
 
@@ -43,6 +44,9 @@ class Auth_Model(DataStore):
 
 	def get_user(self, email_address):
 		return self.user_collection.find_one({'email': email_address })
+
+	def set_strava_token(self, athlete):
+		self.user_collection.update_one({'email': athlete['email']}, {'$set' : {'strava_token': athlete['token']}}, upsert=False)
 
 
 if __name__ == '__main__':
